bf96cb2c24c936486a5c810ced218729d349a9e2
Sarah Brodwall <sarah.brodwall@ehelse.no>
2019-05-02 23:52:42 +0200

    Search page styling

---
+++ _site/assets/js/code-system/resizableSplitter.js
@@ -0,0 +1,13 @@
+function resizableSplitter() {
+
+    var resizablePanel = $(".js-resizable-splitter");
+
+    if (resizablePanel.length !== 0 ) {
+        resizablePanel.resizable({
+            handleSelector: ".splitter",
+            resizeHeight: false
+        });
+    }
+}
+
+window.resizableSplitter = resizableSplitter;
