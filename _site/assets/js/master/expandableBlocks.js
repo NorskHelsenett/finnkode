b9e8a4ef7cd6e7eda2a227d2e4dbf2c5f4d75199
Steffen Lien <steffen.lien@mimedi.no>
2019-05-22 13:47:51 +0200

    Add CircleCI deployment (#1)
    * Restructure and move project files and folders to better suit development
    
    * Add circleci config file to project
    
    * Fix bundler 2 problems occuring on CircleCI
    
    * Comment out HTMLProofer tests
    
    * Add Azure deployment to CircleCI config file
    
    * Fix invalid escaping for CircleCI config file
    
    * Indent the CircleCI config file correctly
    
    * Remove read -sp command from the CircleCI config file
    
    * Remove unused code from config file
    
    * Add the actualy upload command to azure
    
    * Change filter branch to dev branch
    
    * Fix SCSS files not bundling properly
    
    * Add circleci branch as filter temporarily to test the deployment
    
    * Change back branch filter to dev
    
    * Remove Gemfile.lock which wasn't supposed to be in the src folder

--- _site/assets/js/master/expandableBlocks.js
+++
@@ -1,8 +0,0 @@
-// Adds collapsing/expanding functionality to non-responsive expandable blocks.
-
-function expandableBlocks() {
-    var expandableBlocks = $('.js-expand').getExpandableBlocks();
-    expandableBlocks.addExpandability();
-}
-
-window.expandableBlocks = expandableBlocks;
