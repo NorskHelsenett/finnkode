7957d8362e46ab68a768f4231a33c117776780f8
Sarah Brodwall <sjbrodwall@gmail.com>
2019-08-07 01:25:02 +0200

    SVG, footer, and alignment improvements - Fixed textSizeExpander.js to close the expander on Esc - Fixed hover/focus design on all interactive elements in the footer - Fixed footer alignment to grid - Fixed SVGs in IE

--- src/js/master/textSizeExpander.js
+++ src/js/master/textSizeExpander.js
@@ -1,11 +1,21 @@
-﻿// Used to fix issues with expand/collapse on text size button when using keyboard
+﻿// Keyboard functionality for the text size expander in the footer
 function textSizeExpander() {
     let isActive = false;
+
     $('.text-size button').on('click', function() {
         isActive = !isActive;
     });
+
+    // Close the expander when tabbing out
     $('.text-size button').focusout(function(e) {
         if(isActive) {
+            $(this).trigger('click');
+        }
+    });
+
+    // Close the expander when Esc is pressed
+    $('.text-size button').on('keyup', function(e) {
+        if (e.keyCode == 27 && isActive) {
             $(this).trigger('click');
         }
     });
