8543d7a28dbdb6c8c1782b603ba05a2430d6ff93
Sarah Brodwall <sjbrodwall@gmail.com>
2019-10-28 20:55:25 +0100

    Updating fase3 with changes from dev (#37) (#39)
    * fixed confusing case issue with treeitemLinks vs treeItemLinks
    
    * SVG, footer, and alignment improvements
    - Fixed textSizeExpander.js to close the expander on Esc
    - Fixed hover/focus design on all interactive elements in the footer
    - Fixed footer alignment to grid
    - Fixed SVGs in IE
    
    * File regen
    
    * Missing h1 on front and global search pages
    
    * Focus design for links and form elements
    
    * Labels for navs
    
    * Cleaning up JS.  Restoring missing layoutQ on start. Changed markup for code tabs to connect expandable panels.
    
    * Main design for 1- and 2-col code cards
    
    * Hover and focus designs for tabs
    
    * Code system header now minimizes on scroll up in 1- and 2-col
    
    * Design for Administrative kodeverk cards
    
    * Design for download button
    
    * More design and card examples.
    
    * #30 Error and warning messages for failed AJAX calls.
    
    * Fixed a few glaring IE errors
    
    * Mappings/changes table design
    
    * New icons for tables
    
    * Update to code values table design
    
    * Design for expired codes checkbox on code system page

--- src/js/code-system/stickyHeader.js
+++ src/js/code-system/stickyHeader.js
@@ -5,11 +5,61 @@ function stickyHeader() {
     var header = $(".js-sticky-header");
 
     if (header.length > 0) {
-        if (layoutQ().number[0] <= 3) {
-            header.addClass("sticky");
+        header.removeClass("minimal");
+        $(".js-code-system-content-container").off("scroll.stickyHeader");
 
-        } else {
-            header.removeClass("sticky");
+        if (layoutQ().number[0] <= 2) {
+
+            var headerHeight = header.height(),
+                scrollQ = {direction: ["down", "down"], changed: false},
+                previousScrollTop = 0,
+                minScroll = 10,
+                currentDirection = "down",
+                previousDirection = "down";
+
+            $(".js-code-system-content-container").on("scroll.stickyHeader", debounce(function () {
+
+                        scrollQ.changed = false;
+
+                        try {
+                            var currentScrollTop = $(this).scrollTop();
+
+                            if (Math.abs(previousScrollTop - currentScrollTop) <= minScroll)
+                                return;
+
+                            if (currentScrollTop > previousScrollTop) {
+                                currentDirection = "down";
+                                header.addClass("minimal");
+                            } else if (currentScrollTop <= previousScrollTop) {
+                                currentDirection = "up";
+                            }
+
+                            previousScrollTop = currentScrollTop;
+                            previousDirection = currentDirection;
+
+                        } catch (e) {
+                            console.log(e);
+                        }
+
+                        if (scrollQ.direction[0] !== currentDirection) {
+                            scrollQ.direction.unshift(currentDirection);
+                        }
+
+                        if (scrollQ.direction.length > 2) {
+                            scrollQ.changed = true;
+                            scrollQ.direction.pop();
+                        }
+
+                        if (scrollQ.changed) {
+                            if (currentDirection == "down") {
+                                header.addClass("minimal");
+                            } else {
+                                header.removeClass("minimal");
+                            }
+                        }
+                    }, 25)
+            );
+
         }
     }
 }
